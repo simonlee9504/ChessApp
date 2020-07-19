929f8506d0a689038a5c405f194f6d0822d48501
Simon Lee <simonlee9504@gmail.com>
2020-07-19 12:57:08 -0400

    Initial Commit

---
+++ ChessApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChessApp
+//
+//  Created by Simon Lee on 2020-07-19.
+//  Copyright © 2020 Simon Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
